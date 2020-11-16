764c26d2f3117e84227bbb5a471b34d802722aa5
Fodor Franciska <fodorf@HEB.hu>
2020-11-16 17:18:12 +0100

    Spring boot basic project

---
+++ ImageViewer/src/main/java/hu/vir/ImageviewerApplication.java
@@ -0,0 +1,13 @@
+package hu.vir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageviewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageviewerApplication.class, args);
+    }
+
+}
---
+++ ImageViewer/src/test/java/hu/vir/ImageviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.vir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageviewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
